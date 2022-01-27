82dd211f71f005865a1d45313776d07dbe32058a
dgmaghini <dmaghini@gmail.com>
2022-01-27 10:43:36 -0800

    Pooled rep paired donor alpha div analysis

--- scripts/heatmapExample.R
+++ scripts/heatmapExample.R
@@ -1,7 +1,18 @@
 library(ggplot2)
+library(tidyverse)
+library(paletteer)
+library(ggsignif)
 library(here)
 library(RColorBrewer)
+library(reshape2)
+library(metagenomeSeq)
+library(vegan)
+library(cowplot)
+library(ggpubr)
+library(PairedData)
 
+
+##### HEATMAP #####
 results <- read.table(here("test/maaslin_test.txt"), sep="\t", header=TRUE)
 results <- mutate(results, signif=ifelse(qval < 0.05, "TRUE", "FALSE"))
 limit <- max(abs(results$coef)) * c(-1, 1)
@@ -51,3 +62,144 @@ testdf %>%
   group_by(donor, condition) %>%
   summarise_at(vars(diversity), list(name = mean))
 
+
+#####Shannon Diversity Plots#####
+benchmark_s <- read.table(here("DNA/2.kraken/kraken2_classification/processed_results/taxonomy_matrices_classified_only/bracken_genus_percentage.txt"), sep="\t")
+benchmark_groups <- read.csv(here("data/DNAExtraction.tsv"), sep="\t", header=TRUE)
+#Separate the SampleID name into Donor, Condition and Replicate columns; remove=FALSE keeps the SampleID column
+benchmark_groups <- benchmark_groups %>% separate(SampleID, c("Donor", "Condition", "Replicate"), remove=FALSE)
+benchmark_groups <- mutate(benchmark_groups, sample=gsub("_", ".", SampleID))
+benchmark_groups <- filter(benchmark_groups, Donor!="NCO" & Donor!="PCO")
+benchmark_groups <- mutate(benchmark_groups, Preservation=substr(Condition,1,1))
+benchmark_groups <- mutate(benchmark_groups, Temperature=substr(Condition,2,2))
+
+
+shannon_div_s <- diversity(t(benchmark_s), index = "shannon")
+div <- data.frame("shannon_div" = shannon_div_s, "sample" = names(shannon_div_s))
+div_meta <- merge(div, benchmark_groups, by = "sample")
+div_meta <- filter(div_meta, sample != "D03.ZH.R2")
+
+names(div_meta)
+div_grouped_replicates <- div_meta %>% dplyr::select(sample, shannon_div, SampleID, Donor, Condition, Replicate, Preservation, Temperature) %>%
+  group_by(Donor, Condition) %>%
+  summarise_at(vars(shannon_div), list(shannon_div = mean))
+
+div_grouped_replicates <- mutate(div_grouped_replicates, Preservation=substr(Condition,1,1))
+div_grouped_replicates <- mutate(div_grouped_replicates, Temperature=substr(Condition,2,2))
+
+condition_palette <- c("#7c1836","#a22a5e","#c36599","#acaaaf","#bfcd6e","#9dad34","#85950f")
+names(condition_palette) <- c("OH", "OR", "OF", "NF", "ZF", "ZR", "ZH")
+
+div_meta <- div_meta %>% mutate(Condition = fct_relevel(Condition, "NF", "OF", "OR", "OH", "ZF", "ZR", "ZH"))
+div_grouped_replicates <- div_grouped_replicates %>% mutate(Condition = fct_relevel(Condition, "NF", "OF", "OR", "OH", "ZF", "ZR", "ZH"))
+
+# DIVERSITY PLOT BY CONDITION, all replicates
+# ggplot(div_meta, aes(Condition, shannon_div)) + 
+#   geom_jitter(position = position_jitterdodge(jitter.width = 0.8),
+#               alpha = 0.85, aes(fill=Condition), color = "darkgray") +
+#   geom_boxplot(outlier.shape = NA, aes(fill = Condition), alpha = 0.7) + 
+#   labs(x = "",
+#        y = "Shannon Diversity", 
+#        fill = "") + 
+#   scale_color_manual(values = condition_palette) + 
+#   scale_fill_manual(values = condition_palette) +
+#   theme_cowplot(12) +
+#   theme(axis.text.x = element_text(size = 12),
+#         legend.position = "none",
+#         plot.margin = unit(c(0.2, 0.5, 0, 0.2), unit = "cm")) 
+
+# # DIVERSITY PLOT BY CONDITION, mean of replicates
+# ggplot(div_grouped_replicates, aes(Condition, shannon_div)) + 
+#   geom_jitter(position = position_jitterdodge(jitter.width = 0.8),
+#               alpha = 0.85, aes(fill=Condition), color = "darkgray") +
+#   geom_boxplot(outlier.shape = NA, aes(fill = Condition), alpha = 0.7) + 
+#   labs(x = "",
+#        y = "Shannon Diversity", 
+#        fill = "") + 
+#   scale_color_manual(values = condition_palette) + 
+#   scale_fill_manual(values = condition_palette) +
+#   theme_cowplot(12) +
+#   theme(axis.text.x = element_text(size = 12),
+#         legend.position = "none",
+#         plot.margin = unit(c(0.2, 0.5, 0, 0.2), unit = "cm")) 
+
+# # DIVERSITY PLOT BY CONDITION, mean of replicates
+# preserve_effect <- ggplot(div_grouped_replicates %>% filter(Condition %in% c("NF", "OF", "ZF")), aes(Condition, shannon_div)) + 
+#   geom_jitter(position = position_jitterdodge(jitter.width = 0.8),
+#               alpha = 0.85, aes(fill=Condition), color = "darkgray") +
+#   geom_boxplot(outlier.shape = NA, aes(fill = Condition), alpha = 0.7) + 
+#   labs(x = "",
+#        y = "Shannon Diversity", 
+#        fill = "") + 
+#   scale_color_manual(values = condition_palette) + 
+#   scale_fill_manual(values = condition_palette) +
+#   theme_cowplot(12) +
+#   theme(axis.text.x = element_text(size = 12),
+#         legend.position = "none",
+#         plot.margin = unit(c(0.2, 0.5, 0, 0.2), unit = "cm")) 
+# preserve_effect
+
+
+preserve_effect <- ggplot(div_grouped_replicates %>% filter(Condition %in% c("NF", "OF", "ZF")), aes(Condition, shannon_div)) + 
+  geom_jitter(position = position_jitterdodge(jitter.width = 0.8),
+              alpha = 0.85, aes(fill=Condition), color = "darkgray") +
+  geom_boxplot(outlier.shape = NA, aes(fill = Condition), alpha = 0.7) + 
+  labs(x = "",
+       y = "Shannon Diversity", 
+       fill = "", 
+       title = "Preservative Effect") + 
+  ylim(2,4) +
+  scale_color_manual(values = condition_palette) + 
+  scale_fill_manual(values = condition_palette) +
+  theme_cowplot(12) +
+  theme(axis.text.x = element_text(size = 12),
+        legend.position = "none",
+        title = element_text(size = 13),
+        plot.margin = unit(c(0.2, 0.5, 0, 0.2), unit = "cm")) +
+  stat_compare_means(method = "wilcox.test", paired=TRUE, comparisons=list(c("NF", "OF"), c("OF", "ZF"), c("NF", "ZF")), 
+                     tip.length = 0, label = "p.signif")
+preserve_effect
+
+temperature_effect <- ggplot(div_grouped_replicates , aes(Condition, shannon_div)) + 
+  geom_jitter(position = position_jitterdodge(jitter.width = 0.8),
+              alpha = 0.85, aes(fill=Condition), color = "darkgray") +
+  geom_boxplot(outlier.shape = NA, aes(fill = Condition), alpha = 0.7) + 
+  labs(x = "",
+       y = "Shannon Diversity", 
+       fill = "", 
+       title = "Temperature Effect") + 
+  ylim(2,4) +
+  scale_color_manual(values = condition_palette) + 
+  scale_fill_manual(values = condition_palette) +
+  theme_cowplot(12) +
+  theme(axis.text.x = element_text(size = 12),
+        legend.position = "none",
+        title = element_text(size = 13),
+        plot.margin = unit(c(0.2, 0.5, 0, 0.2), unit = "cm")) +
+  stat_compare_means(method = "wilcox.test", paired=TRUE, comparisons=list(c("OF", "OR"), c("OF", "OH"), c("ZF", "ZR"), c("ZF", "ZH")), 
+                     tip.length = 0, label="p.signif")
+temperature_effect
+
+plot_grid(preserve_effect, temperature_effect, rel_widths = c(1,1.9))
+
+ggsave(here("outputs/figures/DNA_alphadiv_paired_by_condition.jpg"), dpi=300, w=8, h=4)
+ggsave(here("outputs/figures/DNA_alphadiv_paired_by_condition.pdf"), dpi=300, w=8, h=4)
+
+
+# ## trying paired plots
+# ggpaired(div_grouped_replicates %>% filter(Temperature == "F"), x="Condition", y="shannon_div",
+#          fill="Condition", line.color="gray", id="Donor",
+#          point.size = 1,
+#          line.size = 1) +
+#   stat_compare_means(paired=TRUE) +
+#   #geom_jitter(position = position_jitterdodge(jitter.width = 0.8),
+#   #            alpha = 0.85, aes(fill=Condition), color = "darkgray") +
+#   scale_color_manual(values = condition_palette) +
+#   scale_fill_manual(values = condition_palette) +
+#   #geom_boxplot(outlier.shape = NA, aes(fill = Condition), alpha = 0.7) +
+#   labs(x = "",
+#        y = "Shannon Diversity",
+#        fill = "") +
+#   theme(axis.text.x = element_text(size = 12),
+#         legend.position = "none",
+#         plot.margin = unit(c(0.2, 0.5, 0, 0.2), unit = "cm"))
