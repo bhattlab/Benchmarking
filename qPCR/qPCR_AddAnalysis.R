affcb12d1de9041e338a1082467da52f36716f20
Mai Dvorak <maidvora@stanford.edu>
2022-08-15 15:39:31 -0700

    Corrected dry stool analysis

--- qPCR/qPCR_AddAnalysis.R
+++ qPCR/qPCR_AddAnalysis.R
@@ -53,3 +53,42 @@ ggplot(donor9, aes(x=DilutionFactor, y=Cq)) +
   scale_x_log10() +
   scale_y_continuous(limits=c(10,30))
 ggsave(here("qPCR/mai/D09ZRR1dilutioncq.jpg"), dpi=300, w = 5, h = 5)
+
+#####Dry Weight Analysis#####
+#Read in dataframe for buffer only weights
+buffer <- read.csv(here("qPCR/mai/PreservativeDryWeight.csv"), sep=",", header=TRUE)
+colnames(buffer)[colnames(buffer) == "X"] <- "SampleName"
+buffer <- buffer %>% separate(SampleName, c("Preservative", "Replicate"), remove=FALSE, sep='_')
+
+#Read in dataframe for stool weights
+stool <- read.csv(here("qPCR/mai/DryStoolWeight.csv"), sep=",", header=TRUE)
+library(data.table)
+stoolt <- transpose(stool)
+rownames(stoolt) <- colnames(stool)
+colnames(stoolt) <- rownames(stool)
+header.true <- function(df) {
+  names(df) <- as.character(unlist(df[1,]))
+  df[-1,]
+}
+stool <-header.true(stoolt)
+library(tibble)
+stool <- tibble::rownames_to_column(stool, "SampleName")
+stool <- stool %>% separate(SampleName, c("Preservative", "Replicate"), remove=FALSE, sep='_')
+
+#Zymo analysis: corrected stool weight with Zymo
+#Zymo: 9mL of buffer to 1mL of stool (10% stool)
+####FIX - use NF wet weight not zymo wet weight
+zcw <- filter(stool, Preservative=="none")
+zcw$Corrected_wet <- as.numeric(zcw$Corrected_wet)
+zcw <- mutate(zcw, ZWeight=(0.1*Corrected_wet)*0.23)
+zcw$ZWeight <- as.numeric(zcw$ZWeight)
+meanzstool <- mean(zcw$ZWeight)
+
+#OMNI analysis: corrected stool weight with OMNIgene
+#OMNI: 2mL of buffer to 520mg (or 0.52mL) stool (21% stool)
+ocw <- filter(stool, Preservative=="none")
+ocw$Corrected_wet <- as.numeric(ocw$Corrected_wet)
+ocw <- mutate(zcw, OWeight=(0.21*Corrected_wet)*0.23)
+ocw$OWeight <- as.numeric(ocw$OWeight)
+meanostool <- mean(ocw$OWeight)
+
